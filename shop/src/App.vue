2d99129301a62faf5892675ee415914fd16081db
Monsieur19 <42003367+Monsieur19@users.noreply.github.com>
2021-03-27 19:46:26 +0300

    4

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,68 +1,73 @@
 <template>
-  <section class="catalog">
-    <catalogList :products="products"/>
-    <ul class="catalog__pagination pagination">
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--arrow pagination__link--disabled"
-        aria-label="Предыдущая страница">
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-left"></use>
-          </svg>
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--current">
-          1
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          2
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          3
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          4
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          ...
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          10
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--arrow" href="#"
-        aria-label="Следующая страница">
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </li>
-    </ul>
-  </section>
+<main class="content container">
+  <div class="content__top content__top--catalog">
+    <h1 class="content__title">
+      Каталог
+    </h1>
+    <span class="content__info">
+      152 товара
+    </span>
+  </div>
+
+  <div class="content__catalog">
+    <ProductFilter :price-from.sync="filterPriceFrom"
+    :price-to.sync="filterPriceTo" :category-id.sync="filterCategoryId"/>
+
+    <section class="catalog">
+      <CatalogList :products="products"/>
+      <BasePagination :page.sync="page" :count="countProducts" :per-page="productsPerPage"/>
+    </section>
+
+  </div>
+</main>
 </template>
 
 <script>
 import products from './data/products';
 import CatalogList from './components/CatalogList.vue';
+import BasePagination from './components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
 
 export default {
   name: 'App',
-  components: { CatalogList },
+  components: { CatalogList, BasePagination, ProductFilter },
   data() {
     return {
-      products,
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+      page: 1,
+      productsPerPage: 3,
     };
+  },
+  computed: {
+    filterProducts() {
+      let filteredProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts
+          .filter((product) => product.price > this.filterPriceFrom);
+      }
+
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts
+          .filter((product) => product.price < this.filterPriceTo);
+      }
+
+      if (this.filterCategoryId) {
+        filteredProducts = filteredProducts
+          .filter((product) => product.categoryId === this.filterCategoryId);
+      }
+
+      return filteredProducts;
+    },
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return this.filterProducts.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return this.filterProducts.length;
+    },
   },
 };
 </script>
---
+++ shop/src/components/CatalogItem.vue
@@ -0,0 +1,47 @@
+<template>
+<li class="catalog__item">
+  <a class="catalog__pic" href="#">
+    <img :src="product.img" alt="Название товара">
+  </a>
+
+  <h3 class="catalog__title">
+    <a href="#">
+      {{product.title}}
+    </a>
+  </h3>
+
+  <span class="catalog__price">
+    {{product.price}}
+  </span>
+
+  <ul class="colors colors--black">
+    <li class="colors__item" v-for="(color) in product.colors" :key="color">
+      <label class="colors__label">
+        <input class="colors__radio sr-only" type="radio" :value="color"
+        :checked="checkColor(color)" @click="changeColor(color)">
+        <span class="colors__value" :style="{backgroundColor: color,}">
+        </span>
+      </label>
+    </li>
+  </ul>
+</li>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+  methods: {
+    checkColor(clr) {
+      return this.color === clr;
+    },
+    changeColor(clr) {
+      this.color = clr;
+    },
+  },
+  data() {
+    return {
+      color: '#73B6EA',
+    };
+  },
+};
+</script>
